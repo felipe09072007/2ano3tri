dbe08c1f8c9c78f1ef7f072d0dc0a9d6d459303b
felipe <132918415+felipe09072007@users.noreply.github.com>
2024-10-21 10:55:06 -0300

    ateração 14

--- perguntas.js
+++ perguntas.js
@@ -15,3 +15,9 @@ criaCartao(
    'O que é uma função?'
    'Uma função é um bloco de código que executa alguma tarefa'
    )
+
+   craiCartao(
+    'lingua inglesa',
+    'como se diz oi em Inglês',
+    'Oi em ingles é HI (RAI)'
+   )
